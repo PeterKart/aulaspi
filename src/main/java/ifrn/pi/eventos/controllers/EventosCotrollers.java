eea2faa6e39d64d202291c655b51a6097cfc5dcd
Pedro Augusto <ricardo.pedro@escolar.ifrn.edu.br>
2023-09-08 17:58:58 -0300

    Modificando para o exercício

--- src/main/java/ifrn/pi/eventos/controllers/EventosCotrollers.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosCotrollers.java
@@ -1,7 +1,12 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosCotrollers {
@@ -10,5 +15,16 @@ public class EventosCotrollers {
 	public String form() {
 		return "formEvento";
 	}
+	  @PostMapping("/processarForm")
+	    public String processarFormulario(@ModelAttribute Evento evento, Model model) {
+	       
+	        System.out.println("Nome: " + evento.getNome());
+	        System.out.println("Descrição: " + evento.getLocal());
+	        System.out.println("Data: " + evento.getData());
+	        System.out.println("Horário: " + evento.getHorario());
 
+	        return "home"; 
+	    }
+
+	
 }
