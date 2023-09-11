43b90069bdbcbcf775cbe1332640d1c1d68c5e7f
PeterKart <143852518+PeterKart@users.noreply.github.com>
2023-09-11 07:49:40 -0300

    Update EventosCotrollers.java

--- src/main/java/ifrn/pi/eventos/controllers/EventosCotrollers.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosCotrollers.java
@@ -1,11 +1,7 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import ifrn.pi.eventos.models.Evento;
 
 @Controller
@@ -15,16 +11,7 @@ public class EventosCotrollers {
 	public String form() {
 		return "formEvento";
 	}
-	  @PostMapping("/processarForm")
-	    public String processarFormulario(@ModelAttribute Evento evento, Model model) {
-	       
-	        System.out.println("Nome: " + evento.getNome());
-	        System.out.println("Descrição: " + evento.getLocal());
-	        System.out.println("Data: " + evento.getData());
-	        System.out.println("Horário: " + evento.getHorario());
-
-	        return "home"; 
-	    }
+	
 
 	
 }
